0df8dae45fcdcb260e4b4ab98892b1d12c76acc1
甲蛙 <zcm101@163.com>
2023-02-04 16:27:03 +0800

    4.18 使用自定义异常处理异常业务

--- common/src/main/java/com/jiawa/train/common/controller/ControllerExceptionHandler.java
+++ common/src/main/java/com/jiawa/train/common/controller/ControllerExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jiawa.train.common.controller;
 
+import com.jiawa.train.common.exception.BusinessException;
 import com.jiawa.train.common.resp.CommonResp;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,12 +23,26 @@ public class ControllerExceptionHandler {
      */
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
-    public CommonResp exceptionHandler(Exception e) throws Exception {
+    public CommonResp exceptionHandler(Exception e) {
         CommonResp commonResp = new CommonResp();
         LOG.error("系统异常：", e);
         commonResp.setSuccess(false);
-        // commonResp.setMessage("系统出现异常，请联系管理员");
-        commonResp.setMessage(e.getMessage());
+        commonResp.setMessage("系统出现异常，请联系管理员");
+        return commonResp;
+    }
+
+    /**
+     * 业务异常统一处理
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public CommonResp exceptionHandler(BusinessException e) {
+        CommonResp commonResp = new CommonResp();
+        LOG.error("业务异常：", e);
+        commonResp.setSuccess(false);
+        commonResp.setMessage(e.getE().getDesc());
         return commonResp;
     }
 
--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.jiawa.train.member.service;
 
 import cn.hutool.core.collection.CollUtil;
+import com.jiawa.train.common.exception.BusinessException;
+import com.jiawa.train.common.exception.BusinessExceptionEnum;
 import com.jiawa.train.member.domain.Member;
 import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
@@ -28,7 +30,7 @@ public class MemberService {
 
         if (CollUtil.isNotEmpty(list)) {
             // return list.get(0).getId();
-            throw new RuntimeException("手机号已注册");
+            throw new BusinessException(BusinessExceptionEnum.MEMBER_MOBILE_EXIST);
         }
 
         Member member = new Member();
