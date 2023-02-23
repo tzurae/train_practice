9534d0937f2ab014358951251e726609584f2203
甲蛙 <zcm101@163.com>
2023-02-23 19:50:18 +0800

    7.6 实现菜单同步激活

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -8,6 +8,7 @@
       </router-link>
     </div>
     <a-menu
+        v-model:selectedKeys="selectedKeys"
         theme="dark"
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
@@ -27,16 +28,24 @@
 </template>
 
 <script>
-import {defineComponent} from 'vue';
+import {defineComponent, ref, watch} from 'vue';
 import store from "@/store";
+import router from '@/router'
 
 export default defineComponent({
   name: "the-header-view",
   setup() {
     let member = store.state.member;
+    const selectedKeys = ref([]);
 
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true});
     return {
-      member
+      member,
+      selectedKeys
     };
   },
 });
