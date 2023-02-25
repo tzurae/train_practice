9be73136c596c829ad7c70cb4585958aa4289a64
甲蛙 <zcm101@163.com>
2023-02-25 17:58:12 +0800

    7.16 解决表格中枚举字段的显示

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -22,6 +22,13 @@
           <a @click="onEdit(record)">编辑</a>
         </a-space>
       </template>
+      <template v-else-if="column.dataIndex === 'type'">
+        <span v-for="item in PASSENGER_TYPE_ARRAY" :key="item.key">
+          <span v-if="item.key === record.type">
+            {{item.value}}
+          </span>
+        </span>
+      </template>
     </template>
   </a-table>
   <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
@@ -48,7 +55,7 @@ import axios from "axios";
 
 export default defineComponent({
   setup() {
-    const PASSENGER_TYPE_ARRAY = [{key: "1", value: "成人1"}, {key: "2", value: "儿童"}, {key: "3", value: "学生"}];
+    const PASSENGER_TYPE_ARRAY = [{key: "1", value: "成人"}, {key: "2", value: "儿童"}, {key: "3", value: "学生"}];
     const visible = ref(false);
     let passenger = ref({
       id: undefined,
