21e9532e2bec15014aeb7a1b01b44009876f8928
甲蛙 <zcm101@163.com>
2023-02-25 15:03:41 +0800

    7.10 乘车人列表查询界面开发，增加loading效果

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -5,7 +5,11 @@
       <a-button type="primary" @click="showModal">新增</a-button>
     </a-space>
   </p>
-  <a-table :dataSource="passengers" :columns="columns" :pagination="pagination" @change="handleTableChange"/>
+  <a-table :dataSource="passengers"
+           :columns="columns"
+           :pagination="pagination"
+           @change="handleTableChange"
+           :loading="loading"/>
   <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
            ok-text="确认" cancel-text="取消">
     <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
@@ -49,6 +53,7 @@ export default defineComponent({
       current: 1,
       pageSize: 2,
     });
+    let loading = ref(false);
     const columns = [{
       title: '姓名',
       dataIndex: 'name',
@@ -86,12 +91,14 @@ export default defineComponent({
           size: pagination.pageSize
         };
       }
+      loading.value = true;
       axios.get("/member/passenger/query-list", {
         params: {
           page: param.page,
           size: param.size
         }
       }).then((response) => {
+        loading.value = false;
         let data = response.data;
         if (data.success) {
           passengers.value = data.content.list;
@@ -128,7 +135,8 @@ export default defineComponent({
       pagination,
       columns,
       handleTableChange,
-      handleQuery
+      handleQuery,
+      loading
     };
   },
 });
