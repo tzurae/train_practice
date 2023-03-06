76e5173a5b83990829338ea79b86fae867234c0f
甲蛙 <zcm101@163.com>
2023-03-06 09:47:25 +0800

    8.9 制作queryResp.ftl模板

--- member/src/main/java/com/jiawa/train/member/resp/PassengerQueryResp.java
+++ member/src/main/java/com/jiawa/train/member/resp/PassengerQueryResp.java
@@ -8,22 +8,43 @@ import java.util.Date;
 
 public class PassengerQueryResp {
 
+    /**
+     * id
+     */
     @JsonSerialize(using= ToStringSerializer.class)
     private Long id;
 
+    /**
+     * 会员id
+     */
     @JsonSerialize(using= ToStringSerializer.class)
     private Long memberId;
 
+    /**
+     * 姓名
+     */
     private String name;
 
+    /**
+     * 身份证
+     */
     private String idCard;
 
+    /**
+     * 旅客类型|枚举[PassengerTypeEnum]
+     */
     private String type;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    /**
+     * 新增时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    /**
+     * 修改时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
