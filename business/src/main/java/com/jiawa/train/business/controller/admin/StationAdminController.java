59e51c42165beffa4f3f2f966226687cca099b51
甲蛙 <zcm101@163.com>
2023-03-07 11:54:55 +0800

    9.6 修改生成器，生成admin相关的代码，测试成功

--- business/src/main/java/com/jiawa/train/business/controller/admin/StationAdminController.java
+++ business/src/main/java/com/jiawa/train/business/controller/admin/StationAdminController.java
@@ -1,18 +1,19 @@
-package com.jiawa.train.business.controller;
+package com.jiawa.train.business.controller.admin;
 
+import com.jiawa.train.common.context.LoginMemberContext;
+import com.jiawa.train.common.resp.CommonResp;
+import com.jiawa.train.common.resp.PageResp;
 import com.jiawa.train.business.req.StationQueryReq;
 import com.jiawa.train.business.req.StationSaveReq;
 import com.jiawa.train.business.resp.StationQueryResp;
 import com.jiawa.train.business.service.StationService;
-import com.jiawa.train.common.resp.CommonResp;
-import com.jiawa.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/station")
-public class StationController {
+@RequestMapping("/admin/station")
+public class StationAdminController {
 
     @Resource
     private StationService stationService;
