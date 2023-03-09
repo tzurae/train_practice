9d6f3d07e92da10e0a4eaf8b063183906d656ed7
甲蛙 <zcm101@163.com>
2023-03-09 12:07:42 +0800

    9.7 增加火车表，生成持久层代码及前后端代码

--- generator/src/main/java/com/jiawa/train/generator/gen/EnumGenerator.java
+++ generator/src/main/java/com/jiawa/train/generator/gen/EnumGenerator.java
@@ -2,6 +2,7 @@ package com.jiawa.train.generator.gen;
 
 
 import cn.hutool.core.util.StrUtil;
+import com.jiawa.train.business.enums.TrainTypeEnum;
 import com.jiawa.train.member.enums.PassengerTypeEnum;
 
 import java.io.FileOutputStream;
@@ -9,7 +10,7 @@ import java.io.OutputStreamWriter;
 import java.lang.reflect.Method;
 
 public class EnumGenerator {
-    static String path = "web/src/assets/js/enums.js";
+    static String path = "admin/src/assets/js/enums.js";
 
     public static void main(String[] args) {
         StringBuffer bufferObject = new StringBuffer();
@@ -17,6 +18,7 @@ public class EnumGenerator {
         long begin = System.currentTimeMillis();
         try {
             toJson(PassengerTypeEnum.class, bufferObject, bufferArray);
+            toJson(TrainTypeEnum.class, bufferObject, bufferArray);
 
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
