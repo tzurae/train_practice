d15fbf480761db7007559cba69364d072a3ee28c
甲蛙 <zcm101@163.com>
2023-03-18 15:18:12 +0800

    9.14 制作车站下拉框组件

--- business/src/main/java/com/jiawa/train/business/controller/admin/StationAdminController.java
+++ business/src/main/java/com/jiawa/train/business/controller/admin/StationAdminController.java
@@ -1,15 +1,16 @@
 package com.jiawa.train.business.controller.admin;
 
-import com.jiawa.train.common.context.LoginMemberContext;
-import com.jiawa.train.common.resp.CommonResp;
-import com.jiawa.train.common.resp.PageResp;
 import com.jiawa.train.business.req.StationQueryReq;
 import com.jiawa.train.business.req.StationSaveReq;
 import com.jiawa.train.business.resp.StationQueryResp;
 import com.jiawa.train.business.service.StationService;
+import com.jiawa.train.common.resp.CommonResp;
+import com.jiawa.train.common.resp.PageResp;
 import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin/station")
@@ -34,6 +35,12 @@ public class StationAdminController {
     public CommonResp<Object> delete(@PathVariable Long id) {
         stationService.delete(id);
         return new CommonResp<>();
+    }
+
+    @GetMapping("/query-all")
+    public CommonResp<List<StationQueryResp>> queryList() {
+        List<StationQueryResp> list = stationService.queryAll();
+        return new CommonResp<>(list);
     }
 
 }
