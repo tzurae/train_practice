76cde3c4bcfb8cc796e009bce5eb53427dcd9ad5
chaomeng.zhang <chaomeng.zhang@huifu.com>
2023-04-14 23:19:46 +0800

    12.7 增加预订按钮，点击预订时，跳转到下单页面，并使用sessionStorage传递参数

--- web/src/views/main/ticket.vue
+++ web/src/views/main/ticket.vue
@@ -14,6 +14,7 @@
            :loading="loading">
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
+        <a-button type="primary" @click="toOrder(record)">预订</a-button>
       </template>
       <template v-else-if="column.dataIndex === 'station'">
         {{record.start}}<br/>
@@ -78,6 +79,7 @@ import {notification} from "ant-design-vue";
 import axios from "axios";
 import StationSelectView from "@/components/station-select";
 import dayjs from "dayjs";
+import router from "@/router";
 
 export default defineComponent({
   name: "ticket-view",
@@ -154,6 +156,10 @@ export default defineComponent({
       dataIndex: 'yw',
       key: 'yw',
     },
+    {
+      title: '操作',
+      dataIndex: 'operation',
+    },
     ];
 
 
@@ -214,6 +220,12 @@ export default defineComponent({
       return dayjs('00:00:00', 'HH:mm:ss').second(diff).format('HH:mm:ss');
     };
 
+    const toOrder = (record) => {
+      dailyTrainTicket.value = Tool.copy(record);
+      SessionStorage.set("dailyTrainTicket", dailyTrainTicket.value);
+      router.push("/order")
+    };
+
     onMounted(() => {
       // handleQuery({
       //   page: 1,
@@ -231,7 +243,8 @@ export default defineComponent({
       handleQuery,
       loading,
       params,
-      calDuration
+      calDuration,
+      toOrder
     };
   },
 });
