10bcaf826dca588c1e5967f59ac1735e61e19362
甲蛙 <82144921@qq.com>
2023-04-22 08:16:20 +0800

    12.12 后端增加确认订单接口，实现前后端交互

--- business/src/main/java/com/jiawa/train/business/controller/admin/ConfirmOrderAdminController.java
+++ business/src/main/java/com/jiawa/train/business/controller/admin/ConfirmOrderAdminController.java
@@ -1,10 +1,9 @@
 package com.jiawa.train.business.controller.admin;
 
-import com.jiawa.train.common.context.LoginMemberContext;
 import com.jiawa.train.common.resp.CommonResp;
 import com.jiawa.train.common.resp.PageResp;
 import com.jiawa.train.business.req.ConfirmOrderQueryReq;
-import com.jiawa.train.business.req.ConfirmOrderSaveReq;
+import com.jiawa.train.business.req.ConfirmOrderDoReq;
 import com.jiawa.train.business.resp.ConfirmOrderQueryResp;
 import com.jiawa.train.business.service.ConfirmOrderService;
 import jakarta.annotation.Resource;
@@ -19,7 +18,7 @@ public class ConfirmOrderAdminController {
     private ConfirmOrderService confirmOrderService;
 
     @PostMapping("/save")
-    public CommonResp<Object> save(@Valid @RequestBody ConfirmOrderSaveReq req) {
+    public CommonResp<Object> save(@Valid @RequestBody ConfirmOrderDoReq req) {
         confirmOrderService.save(req);
         return new CommonResp<>();
     }
