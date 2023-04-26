609d96422172c8215eb1b2076c5bc6e4fad09554
甲蛙 <82144921@qq.com>
2023-04-26 17:25:03 +0800

    12.22 增加乘客车票表，生成前后端代码

---
+++ member/src/main/java/com/jiawa/train/member/controller/admin/TicketAdminController.java
@@ -0,0 +1,39 @@
+package com.jiawa.train.member.controller.admin;
+
+import com.jiawa.train.common.context.LoginMemberContext;
+import com.jiawa.train.common.resp.CommonResp;
+import com.jiawa.train.common.resp.PageResp;
+import com.jiawa.train.member.req.TicketQueryReq;
+import com.jiawa.train.member.req.TicketSaveReq;
+import com.jiawa.train.member.resp.TicketQueryResp;
+import com.jiawa.train.member.service.TicketService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/ticket")
+public class TicketAdminController {
+
+    @Resource
+    private TicketService ticketService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody TicketSaveReq req) {
+        ticketService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<TicketQueryResp>> queryList(@Valid TicketQueryReq req) {
+        PageResp<TicketQueryResp> list = ticketService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        ticketService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
