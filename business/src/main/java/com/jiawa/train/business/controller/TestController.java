df44685213d74d6603952824b69954218ab1f434
甲蛙 <82144921@qq.com>
2023-05-17 23:05:49 +0800

    17.8 修改sentinel与nacos相关配置

--- business/src/main/java/com/jiawa/train/business/controller/TestController.java
+++ business/src/main/java/com/jiawa/train/business/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.jiawa.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @SentinelResource("hello")
     @GetMapping("/hello")
-    public String hello() {
+    public String hello() throws InterruptedException {
+        // Thread.sleep(500);
         return "Hello World! Business!";
     }
+
 }
