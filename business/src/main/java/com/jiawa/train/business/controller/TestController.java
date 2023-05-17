86178dd87e305e6016da4bd7144c6fbe9de29a53
甲蛙 <82144921@qq.com>
2023-05-17 23:20:21 +0800

    17.9 Sentinel限流不同的流控模式讲解 - 关联&链路

--- business/src/main/java/com/jiawa/train/business/controller/TestController.java
+++ business/src/main/java/com/jiawa/train/business/controller/TestController.java
@@ -1,17 +1,29 @@
 package com.jiawa.train.business.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.jiawa.train.business.service.TestService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Resource
+    private TestService testService;
+
     @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello() throws InterruptedException {
-        // Thread.sleep(500);
+        testService.hello2();
         return "Hello World! Business!";
+    }
+
+    @SentinelResource("hello1")
+    @GetMapping("/hello1")
+    public String hello1() throws InterruptedException {
+        testService.hello2();
+        return "Hello World! Business1!";
     }
 
 }
