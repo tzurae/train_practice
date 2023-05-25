1cfce9f31020c32a6a3ba47e1804726df7dd712f
甲蛙 <82144921@qq.com>
2023-05-25 18:11:41 +0800

    19.6 下单购票接口，只处理验证码、令牌、车次锁，不执行选座购票逻辑

--- business/src/main/java/com/jiawa/train/business/controller/ConfirmOrderController.java
+++ business/src/main/java/com/jiawa/train/business/controller/ConfirmOrderController.java
@@ -3,7 +3,7 @@ package com.jiawa.train.business.controller;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.jiawa.train.business.req.ConfirmOrderDoReq;
-import com.jiawa.train.business.service.ConfirmOrderService;
+import com.jiawa.train.business.service.BeforeConfirmOrderService;
 import com.jiawa.train.common.exception.BusinessExceptionEnum;
 import com.jiawa.train.common.resp.CommonResp;
 import jakarta.annotation.Resource;
@@ -25,7 +25,7 @@ public class ConfirmOrderController {
     private static final Logger LOG = LoggerFactory.getLogger(ConfirmOrderController.class);
 
     @Resource
-    private ConfirmOrderService confirmOrderService;
+    private BeforeConfirmOrderService beforeConfirmOrderService;
 
     @Autowired
     private StringRedisTemplate redisTemplate;
@@ -51,7 +51,7 @@ public class ConfirmOrderController {
             redisTemplate.delete(imageCodeToken);
         }
 
-        confirmOrderService.doConfirm(req);
+        beforeConfirmOrderService.beforeDoConfirm(req);
         return new CommonResp<>();
     }
 
