04d0d1b1c616a38067fbe7001df6f7a6ca56454e
甲蛙 <82144921@qq.com>
2023-06-05 13:26:24 +0800

    22.5 显示各车厢各座位的销售详情，使用橙色灰色代码座位可卖与卖出

--- business/src/main/java/com/jiawa/train/business/resp/SeatSellResp.java
+++ business/src/main/java/com/jiawa/train/business/resp/SeatSellResp.java
@@ -13,9 +13,9 @@ public class SeatSellResp {
     private String row;
 
     /**
-     * 列号|枚举[SeatColumnEnum]
+     * 列号|枚举[SeatColEnum]
      */
-    private String column;
+    private String col;
 
     /**
      * 座位类型|枚举[SeatTypeEnum]
@@ -43,12 +43,12 @@ public class SeatSellResp {
         this.row = row;
     }
 
-    public String getColumn() {
-        return column;
+    public String getCol() {
+        return col;
     }
 
-    public void setColumn(String column) {
-        this.column = column;
+    public void setCol(String col) {
+        this.col = col;
     }
 
     public String getSeatType() {
@@ -75,7 +75,7 @@ public class SeatSellResp {
         sb.append("Hash = ").append(hashCode());
         sb.append(", carriageIndex=").append(carriageIndex);
         sb.append(", row=").append(row);
-        sb.append(", column=").append(column);
+        sb.append(", col=").append(col);
         sb.append(", seatType=").append(seatType);
         sb.append(", sell=").append(sell);
         sb.append("]");
